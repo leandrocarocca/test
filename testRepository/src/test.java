608017512882646f838baa428faba598b6bc46ab
lcj <lcj@kth.se>
2022-11-20 02:39:30 +0100

    added function printInteger

--- testRepository/src/test.java
+++ testRepository/src/test.java
@@ -1,8 +1,11 @@
 public class test
 {
+    public static void printInteger(int i){
+        System.out.println(i);
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Testing");
-        // Another change
+        printInteger(3);
     }
 }
