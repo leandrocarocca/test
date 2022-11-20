b11612142fbc8945824bfbb061ed288afaff3b97
lcj <lcj@kth.se>
2022-11-20 02:42:57 +0100

    added function sayHello

--- testRepository/src/test.java
+++ testRepository/src/test.java
@@ -4,8 +4,14 @@ public class test
         System.out.println(i);
     }
 
+    public static void sayHello(){
+        System.out.println("Hello!");
+    }
     public static void main(String[] args)
     {
         printInteger(3);
+        printInteger(4);
+        printInteger(5);
+        sayHello();
     }
 }
