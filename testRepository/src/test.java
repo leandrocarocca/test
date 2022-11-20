2e61254a6583649a17928d78d6bba9b0ecb71698
leandrocarocca <114980843+leandrocarocca@users.noreply.github.com>
2022-11-20 02:40:27 +0100

    Update test.java

--- testRepository/src/test.java
+++ testRepository/src/test.java
@@ -1,11 +1,11 @@
 public class test
 {
-    public static void printInteger(int i){
+    public static void getInteger(int i){
         System.out.println(i);
     }
 
     public static void main(String[] args)
     {
-        printInteger(3);
+        
     }
 }
